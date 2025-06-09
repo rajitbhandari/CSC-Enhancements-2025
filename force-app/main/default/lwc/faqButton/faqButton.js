59b246a7dc81dafab255ec1e31c80355c3dc445a
Rohith Mathew Varghese <romathewvarghese@deloitte.com>
2025-06-09 13:47:06 -0400

    add faq button

---
+++ force-app/main/default/lwc/faqButton/faqButton.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+
+export default class FaqButton extends LightningElement {
+    handleNavigation() {
+    window.location.href='https://nccdphp--qa.sandbox.my.site.com/s/topic/0TOt0000000H3w3GAC/frequently-asked-questions-faqs'; 
+    }
+    }
