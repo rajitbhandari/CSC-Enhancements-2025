8a27299daa25e44f4eb4141c760f35b3548dd16a
Rohith Mathew Varghese <romathewvarghese@deloitte.com>
2025-06-09 17:29:34 -0400

    updated changes

--- force-app/main/default/lwc/cscTileMenu/cscTileMenu.js
+++ force-app/main/default/lwc/cscTileMenu/cscTileMenu.js
@@ -1,34 +1,17 @@
 import { LightningElement } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 
-export default class CscTileMenu extends NavigationMixin(LightningElement) {
+export default class CscTileMenu extends LightningElement  {
     cscButton(event){
         window.open(window.location.href.substring(0,
             window.location.href.indexOf("/s/") + 3) +
             "about-the-customer-service-center","_blank");
     }
-    async discussionBoard(event){
-        event.preventDefault();
-        // Check login status by attempting to get the user Id from the session
-        let isLoggedIn = false;
-        try {
-            // This will throw if not logged in
-            const result = await fetch('/services/data/v59.0/chatter/users/me', { credentials: 'same-origin' });
-            if (result.ok) {
-                isLoggedIn = true;
-            }
-        } catch (e) {
-            isLoggedIn = false;
+    discussionBoard(event){
+        window.open(window.location.href.substring(0,
+                                      window.location.href.indexOf("/s/") + 3) +
+                                      "discussion-board", "_blank");
         }
-        if (isLoggedIn) {
-            window.open(window.location.href.substring(0,
-                window.location.href.indexOf("/s/") + 3) +
-                "discussion-board", "_blank");
-        } else {
-            // Redirect to login page
-            window.location.href = '/s/login';
-        }
-    }
     contactSupport(event){
         window.open(window.location.href.substring(0,
             window.location.href.indexOf("/s/") + 3) +
