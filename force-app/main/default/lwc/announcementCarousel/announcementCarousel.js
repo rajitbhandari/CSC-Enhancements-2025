5f8255c16b98dfd1fcf535171af713b11088e109
christina.willy@dhs.wisconsin.gov <cwilly@deloitte.com>
2025-08-18 12:33:49 -0500

    Recent changes from QA

--- force-app/main/default/lwc/announcementCarousel/announcementCarousel.js
+++ force-app/main/default/lwc/announcementCarousel/announcementCarousel.js
@@ -1,10 +1,67 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement } from 'lwc';
 
 export default class AnnouncementCarousel extends LightningElement {
-    @track hasUpcomingEvents = true;
+    slides = [
+        {
+            id: 'slide-1',
+            href: 'https://nationaldppcsc.cdc.gov/s/article/in-person-discovery-session-guide',
+            img: '/resource/1737063718000/DiscoverySessionInPerson?',
+            alt: 'Discovery Session for in person delivery',
+            title: 'Discovery Session for in person delivery',
+            indicatorLabel: 'Show slide 1: Discovery Session for in person delivery'
+        },
+        {
+            id: 'slide-2',
+            href: 'https://nationaldppcsc.cdc.gov/s/article/Session-Zero-Guide-for-Live-Videoconferencing',
+            img: '/resource/1737419531000/DiscoverySession_Videoconferencing?',
+            alt: 'Discovery Session for videoconferencing',
+            title: 'Discovery Session for videoconferencing',
+            indicatorLabel: 'Show slide 2: Discovery Session for videoconferencing'
+        }
+    ];
 
-    handleUpcomingEventsLoaded(event) {
-        // event.detail should be the number of events from the child
-        this.hasUpcomingEvents = event.detail > 0;
+    currentIndex = 0;
+    intervalId;
+
+    connectedCallback() {
+        this.startAutoAdvance();
+    }
+
+    disconnectedCallback() {
+        this.stopAutoAdvance();
+    }
+
+    startAutoAdvance() {
+        this.intervalId = setInterval(() => {
+            this.nextSlide();
+        }, 5000); // Change slide every 5 seconds
+    }
+
+    stopAutoAdvance() {
+        if (this.intervalId) {
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+        }
+    }
+
+    nextSlide() {
+        this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+    }
+
+    goToSlide(event) {
+        const idx = Number(event.target.dataset.index);
+        this.currentIndex = idx;
+        this.stopAutoAdvance();
+        this.startAutoAdvance();
+    }
+
+    get computedSlides() {
+        return this.slides.map((slide, idx) => ({
+            ...slide,
+            isActive: idx === this.currentIndex,
+            ariaHidden: idx === this.currentIndex ? 'false' : 'true',
+            indicatorId: `indicator-${idx + 1}`,
+            tabIndex: idx === this.currentIndex ? "0" : "-1"
+        }));
     }
 }
--- force-app/main/default/lwc/cscTileMenu/cscTileMenu.js
+++ force-app/main/default/lwc/cscTileMenu/cscTileMenu.js
@@ -1,31 +1,20 @@
 import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class CscTileMenu extends LightningElement {
+export default class CscTileMenu extends LightningElement  {
     cscButton(event){
         window.open(window.location.href.substring(0,
             window.location.href.indexOf("/s/") + 3) +
             "about-the-customer-service-center","_blank");
-        /*window.location.assign(
-        window.location.href.substring(0,
-                                      window.location.href.indexOf("/s/") + 3) +
-                                      "about-the-customer-service-center");*/
-       }
-       discussionBoard(event){
+    }
+    discussionBoard(event){
         window.open(window.location.href.substring(0,
                                       window.location.href.indexOf("/s/") + 3) +
                                       "discussion-board", "_blank");
-        /*window.location.assign(
-        window.location.href.substring(0,
-                                      window.location.href.indexOf("/s/") + 3) +
-                                      "discussion-board");*/
-       }
-       contactSupport(event){
+        }
+    contactSupport(event){
         window.open(window.location.href.substring(0,
             window.location.href.indexOf("/s/") + 3) +
             "contactsupport","_blank");
-        /*window.location.assign(
-        window.location.href.substring(0,
-                                      window.location.href.indexOf("/s/") + 3) +
-                                      "contactsupport");*/
-       }
+    }
 }
